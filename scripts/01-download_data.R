274f2e12112828b18356a38a8de7dcc72bf206f9
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-15 20:19:41 +0000

    Removed sample code and added my own preamble and library set up sections

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,13 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves data from US General Social Survey
+# Author: Inessa De Angelis
+# Date: 12 June 2023
+# Contact: inessa.deangelis@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Data set: https://gss.norc.org/ 
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
-
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+library(haven)
