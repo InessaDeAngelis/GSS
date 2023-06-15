696e2da84d34a887344005c42440080ab83b9bed
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-15 20:30:02 +0000

    Added code I previously wrote to download the raw GSS data and zip the files, all while hoping the Calgary airport wifi was strong enough

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -11,3 +11,22 @@
 #### Workspace setup ####
 library(tidyverse)
 library(haven)
+
+#### Download data ####
+# Download & unzip GSS data from all years
+zip_file <- "inputs/data/large_files/GSS_stata.zip"
+
+download.file("https://gss.norc.org/documents/stata/GSS_stata.zip", zip_file)
+unzip(zip_file, exdir = "inputs/data/large_files")
+
+# read dta data and write to csv
+raw_gss_data <- read_dta("inputs/data/large_files/gss7222_r1.dta")
+
+# filter survey data and select relevant data
+raw_respondent_info <-
+  raw_gss_data |> 
+  select(
+    year,
+    id,
+  )
+ 
