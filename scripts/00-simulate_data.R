1165e29e9de8bdf9cde40605b269933301c5798d
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-18 14:59:23 -0400

    Added expectations for simulated data and added script to simulate political views and party affiliation data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -4,23 +4,51 @@
 # Date: 12 June 2023
 # Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
-
+# Pre-requisites: None 
 
 #### Workspace setup ####
 library(tidyverse)
-library(tibble)
+library(janitor)
 
-#### Simulate data: men better suited emotionally for politics than women ####
-#Code referenced from: https://tellingstorieswithdata.com/08-hunt.html#surveys
+#### Data expectations ####
+# Agreement varies with the statement that men are better suited emotionally for politics than women
+# More people agree with the statement that men are better suited emotionally for politics than women in previous decades
+# Political views vary on a scale from 1-7 from extremely liberal, to moderate, to extremely conservative 
+# Party affiliation varies on a scale from 0-7 from strong Democrat, to independent/neutral, to strong Republican
+# People with more liberal political views will disagree that men are emotionally better suited for politics than women
+
+#### Simulate responses to men being better suited emotionally for politics than women ####
+#Code referenced from: https://tellingstorieswithdata.com/08-hunt.html
 set.seed(416)
 
-treat_control <-
-  tibble(
-    group = sample(x = c("Treatment", "Control"), size = 50, replace = TRUE),
-    response = sample(x = c(1, 2), size = 50, replace = TRUE)
-  )
+num_people <- 5000
 
-treat_control
+population <- tibble(
+  person = 1:num_people,
+  men_better_suited = sample(c("Agree", "Disagree"), size = num_people, replace = TRUE),
+)
+population
 
+#### Simulate varying political views ####
+set.seed(416)
 
+num_people <- 5000
 
+simulated_data <-tibble(
+    person = 1:num_people,
+#use 1 through 7 to represent political views 
+    political_views = sample(c("1", "2", "3", "4", "5", "6", "7"), size = num_people, replace = TRUE),
+)
+simulated_data
+
+#### Simulate varying party affiliation ####
+set.seed(416)
+
+num_people <- 5000
+
+simulated_data <-tibble(
+  person = 1:num_people,
+  #use 0 through 7 to represent political views 
+  party_affiliation = sample(c("0", "1", "2", "3", "4", "5", "6", "7"), size = num_people, replace = TRUE),
+)
+simulated_data
