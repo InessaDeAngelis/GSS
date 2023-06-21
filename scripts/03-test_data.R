eef72b48e2de40902f0319b4050caf3ca227dc92
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-21 17:24:13 -0400

    Updated script preamble, workspace set up and read in the three cleaned data sets.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,22 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
+# Purpose: Tests the three cleaned data sets
+# Author: Inessa De Angelis
 # Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Contact: inessa.deangelis@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Pre-requisites:
+  # 01-download_data.R
+  # 02-data_cleaning.R
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
-#### Test data ####
+#### Read in cleaned data sets ####
+# Read in the cleaned respondent information data # 
+readr::read_csv("outputs/data/cleaned_respondent_info.csv")
+
+# Read in the cleaned women in politics data # 
+readr::read_csv("outputs/data/cleaned_women_in_politics.csv")
+
+# Read in the cleaned & summarized political preferences data # 
+readr::read_csv("outputs/data/summarized_political_preferences.csv")
