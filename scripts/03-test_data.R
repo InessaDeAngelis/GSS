4fdabfa613c13dc3d0d9667c671ff26f332e889f
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-22 09:56:13 -0400

    Added "here" package to the library. Updated script to read in cleaned data sets using the "here" package. Began writing tests to validate the cleaned data sets, starting with a test to check the years included.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,13 +10,22 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(here)
 
 #### Read in cleaned data sets ####
 # Read in the cleaned respondent information data # 
-readr::read_csv("outputs/data/cleaned_respondent_info.csv")
+cleaned_respondent_info <- read.csv(here::here("outputs/data/cleaned_respondent_info.csv"))
+show_col_types = FALSE
 
 # Read in the cleaned women in politics data # 
-readr::read_csv("outputs/data/cleaned_women_in_politics.csv")
+cleaned_women_in_politics <- read.csv(here::here("outputs/data/cleaned_women_in_politics.csv"))
+show_col_types = FALSE
 
 # Read in the cleaned & summarized political preferences data # 
-readr::read_csv("outputs/data/summarized_political_preferences.csv")
+summarized_political_preferences <- read_csv(here::here("outputs/data/summarized_political_preferences.csv"))
+show_col_types = FALSE
+
+#### Validate cleaned data ####
+# Check that responses range from 1974 through 2022 #
+min(cleaned_respondent_info$year) <= 1974
+max(cleaned_respondent_info$year) >= 2022
