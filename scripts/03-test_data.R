6d77e41aaac8028a24827fc635aaa2501013da04
Inessa De Angelis <inessa.deangelis@mail.utoronto.ca>
2023-06-22 11:15:45 -0400

    Wrote script to test min and max years for all three data sets and wrote additional scripts to validate political preferences, party ids, and other aspects of my data sets.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -21,11 +21,76 @@ show_col_types = FALSE
 cleaned_women_in_politics <- read.csv(here::here("outputs/data/cleaned_women_in_politics.csv"))
 show_col_types = FALSE
 
-# Read in the cleaned & summarized political preferences data # 
+# Read in the summarized political preferences data # 
 summarized_political_preferences <- read_csv(here::here("outputs/data/summarized_political_preferences.csv"))
 show_col_types = FALSE
 
-#### Validate cleaned data ####
+#### Validate the cleaned respondent info data ####
 # Check that responses range from 1974 through 2022 #
 min(cleaned_respondent_info$year) <= 1974
 max(cleaned_respondent_info$year) >= 2022
+sum(is.na(cleaned_respondent_info$year)) == 0
+
+# Check that respondent ages range from 18 to 90 #
+class(cleaned_respondent_info$age) == "integer"
+min(cleaned_respondent_info$age, na.rm = TRUE) >= 18
+max(cleaned_respondent_info$age, na.rm = TRUE) <= 90
+
+# Check that the gender of respondents are either male or female #
+class(cleaned_respondent_info$sex) == "character"
+cleaned_respondent_info |>
+  group_by(sex) |>
+ summarise()
+
+#### Validate the cleaned women in politics data ####
+# Check that responses range from 1974 through 2022 #
+min(cleaned_women_in_politics$year) <= 1974
+max(cleaned_women_in_politics$year) >= 2022
+sum(is.na(cleaned_women_in_politics$year)) == 0
+
+# Check that respondents can either agree or disagree about women belonging in politics #
+class(cleaned_women_in_politics$women_in_politics) == "character"
+cleaned_women_in_politics |>
+  group_by(women_in_politics) |>
+  summarise()
+
+#### Validate the summarized political preferences data ####
+# Check that responses range from 1974 through 2022 #
+min(summarized_political_preferences$year) <= 1974
+max(summarized_political_preferences$year) >= 2022
+sum(is.na(summarized_political_preferences$year)) == 0
+
+# Check political views range from Extremely liberal to Extremely conservative # 
+# Code referenced from: https://github.com/christina-wei/Financial-Wellness-in-US/blob/main/scripts/03-test_data.R
+class(summarized_political_preferences$polviews) == "character"
+sum(!(unique(summarized_political_preferences$polviews) %in%
+        c("Extremely Liberal",
+          "Liberal",
+          "Slightly Liberal",
+          "Moderate",
+          "Slightly Conservative",
+          "Conservative",
+          "Extremely Conservative",
+          NA))) == 0
+
+summarized_political_preferences$polviews |>
+  unique() |>
+  length() == 7
+
+# Check party ids range from Strong Democrat to Other # 
+class(summarized_political_preferences$partyid) == "character"
+sum(!(unique(summarized_political_preferences$partyid) %in%
+        c("Strong Democrat",
+          "Not Strong Democrat",
+          "Independent, Close to Democrat",
+          "Independent",
+          "Independent, Close to Republican",
+          "Not Strong Republican",
+          "Strong Republican",
+          "Other",
+          NA))) == 0
+
+summarized_political_preferences$partyid |>
+  unique() |>
+  length() == 8
+  
